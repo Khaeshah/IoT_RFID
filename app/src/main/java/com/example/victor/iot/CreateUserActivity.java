ff59cba396647ba9f256d7abe9c39e43d71fe52a
victorvila92 <viko_bcn_@hotmail.com>
2018-06-10 14:22:49 +0200

    Refactoring

--- app/src/main/java/com/example/victor/iot/CreateUserActivity.java
+++ app/src/main/java/com/example/victor/iot/CreateUserActivity.java
@@ -37,8 +37,6 @@ public class CreateUserActivity extends AppCompatActivity {
         goBack();
     }
 
-
-
     public void sendData(){
         sendBtn.setOnClickListener(
                 new View.OnClickListener() {
